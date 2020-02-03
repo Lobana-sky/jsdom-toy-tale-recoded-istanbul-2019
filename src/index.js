549c80e8a88956d0776d60ad565c89b980e39a55
lobana alarabi <loubanaeng@gmail.com>
2020-02-03 10:02:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -63,8 +63,9 @@ let image=x[1].value;
         "Accept": "application/json"
       },
       body: JSON.stringify( {
-        name,
-        email
+       "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
       } )
     } )
 
